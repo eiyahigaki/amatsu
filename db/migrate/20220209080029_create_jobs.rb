ad596e62b14172e685964e2aa96099dc076c19ea
eiyahigaki <eiyahigaki@gmail.com>
2022-02-09 17:32:07 +0900

    インデントを揃える

--- db/migrate/20220209080029_create_jobs.rb
+++ db/migrate/20220209080029_create_jobs.rb
@@ -1,7 +1,7 @@
 class CreateJobs < ActiveRecord::Migration[6.0]
   def change
     create_table :jobs do |t|
-      t.string     :status,         null: false
+      t.string     :status,            null: false
       t.string     :title,             null: false
       t.integer    :occupaton_id,      null: false
       t.integer    :city,              null: false
